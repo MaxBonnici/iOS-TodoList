cb2353c15969048e7f0bf40da9e07282b0f117c5
Max Bonnici <95021980+MaxBonnici@users.noreply.github.com>
2022-12-15 09:12:30 +0100

    Add files via upload

---
+++ xcode/MonPremierTableView/AddViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBOutlet weak var myTitle: UITextField!
+    @IBOutlet weak var myDescription: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ xcode/MonPremierTableView/model.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+class MyData {
+    var title: String
+    var description: String
+    
+    init(title: String, description: String) {
+        self.title = title
+        self.description = description
+    }
+}
